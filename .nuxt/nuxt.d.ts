b0ed805f8e6787048a2e06c2e3414c4da02917e3
Wailan Tirajoh <wailantirajoh@gmail.com>
2026-05-13 21:36:49 +0700

    feat(app): initialize fresh nuxt with tauri

---
+++ .nuxt/nuxt.d.ts
@@ -0,0 +1,23 @@
+/// <reference types="@pinia/nuxt" />
+/// <reference types="@pinia/colada-nuxt" />
+/// <reference types="@vueuse/nuxt" />
+/// <reference types="@nuxt/devtools" />
+/// <reference types="@nuxt/telemetry" />
+/// <reference path="types/nitro-layouts.d.ts" />
+/// <reference path="types/builder-env.d.ts" />
+/// <reference path="types/plugins.d.ts" />
+/// <reference path="types/build.d.ts" />
+/// <reference path="types/app.config.d.ts" />
+/// <reference path="types/runtime-config.d.ts" />
+/// <reference types="nuxt/app" />
+/// <reference path="../node_modules/.pnpm/@nuxt+nitro-server@4.4.5_@babel+core@7.29.0_db0@0.3.4_ioredis@5.10.1_magicast@0.5.2_nux_551036f9083d956b3cdba14ec3030446/node_modules/@nuxt/nitro-server/dist/index.d.mts" />
+/// <reference types="@pinia/nuxt" />
+/// <reference path="../node_modules/.pnpm/@pinia+colada-nuxt@1.0.1_@pinia+colada@1.3.0_pinia@3.0.4_typescript@5.6.3_vue@3.5.34_ty_a3088cb751ef497af49355e84e63d8ea/node_modules/@pinia/colada-nuxt/dist/types/build.d.ts" />
+/// <reference path="types/middleware.d.ts" />
+/// <reference path="types/components.d.ts" />
+/// <reference path="imports.d.ts" />
+/// <reference path="types/imports.d.ts" />
+/// <reference path="schema/nuxt.schema.d.ts" />
+/// <reference path="types/nitro.d.ts" />
+
+export {}
---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  // ssr: false,
+  vite: {
+    // Better support for Tauri CLI output
+    clearScreen: false,
+    // Enable environment variables
+    // Additional environment variables can be found at
+    // https://v2.tauri.app/reference/environment-variables/
+    envPrefix: ["VITE_", "TAURI_"],
+    server: {
+      // Tauri requires a consistent port
+      strictPort: true,
+    },
+  },
+
+  modules: ["@pinia/nuxt", "@pinia/colada-nuxt", "@vueuse/nuxt"],
+});
